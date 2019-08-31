8661c1df7c0f68a97253abddcf6426717cb24b71
duowen1314 <duo_eweng@163.com>
2019-08-31 16:23:48 +0800

    第六次提交表单验证

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import './assets/index.less'
+import axios from 'axios'
+// 给Vue对象的原型属性赋值
+Vue.prototype.$axios = axios
 Vue.use(ElementUI)
 
 Vue.config.productionTip = false
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -4,16 +4,16 @@
       <div class="title">
         <img src="../../assets/imgs/logo_index.png" alt="">
       </div>
-      <el-form>
-        <el-form-item>
-          <el-input  placeholder="请输入手机号"></el-input>
+      <el-form :model="loginForm" :rules="loginRules">
+        <el-form-item prop="mobile">
+          <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
         </el-form-item>
-        <el-form-item>
-          <el-input placeholder="请输入验证码" style="width:240px"></el-input>
+        <el-form-item prop="code">
+          <el-input v-model="loginForm.code" placeholder="请输入验证码" style="width:240px"></el-input>
           <el-button style="float:right">发送验证码</el-button>
         </el-form-item>
-        <el-form-item>
-           <el-checkbox v-model="checked">我已阅读并同意用户协议和隐私条款</el-checkbox>
+        <el-form-item prop="check">
+           <el-checkbox v-model="loginForm.check">我已阅读并同意用户协议和隐私条款</el-checkbox>
         </el-form-item>
         <el-form-item>
           <el-button type="primary" style="width:100%">主要按钮</el-button>
@@ -25,7 +25,30 @@
 
 <script>
 export default {
-
+  data () {
+    return {
+      // el-form绑定的model属性值
+      loginForm: {
+        mobile: '', // 手机号
+        code: '', // 验证码
+        check: false // 同意
+      },
+      // 定义校验规则
+      loginRules: {
+        mobile: [{
+          required: true,
+          message: '手机号不能为空'
+        }, {
+          pattern: /^1[3456789]\d{9}$/,
+          message: '手机号码格式不正确'
+        }],
+        code: [{
+          required: true,
+          message: '验证码不能为空'
+        }]
+      }
+    }
+  }
 }
 </script>
 
