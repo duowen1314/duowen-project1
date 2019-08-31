891288294b2fc9eabc9f449b04155639bf843640
duowen1314 <duo_eweng@163.com>
2019-08-31 13:53:48 +0800

    第四次提交新建login组件并配置路由表

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/home/home.vue'
+import Home from './views/home/index.vue'
+import Login from './views/login/index.vue'
 
 Vue.use(Router)
 
@@ -8,8 +9,11 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
       component: Home
+    },
+    {
+      path: '/login',
+      component: Login
     }
     // {
     //   path: '/about',
