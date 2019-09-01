1f0bc152e210934e17b6abf2f8fea78930a017d3
duowen1314 <duo_eweng@163.com>
2019-09-01 09:45:13 +0800

    主页布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,15 +1,28 @@
 <template>
   <div>
-      <el-input v-model="input" placeholder="请输入内容"></el-input>
+    <!-- 最外层 -->
+    <el-container>
+      <!-- 左侧 -->
+      <el-aside class="left">
+        左侧
+      </el-aside>
+      <!-- 右侧 -->
+      <el-container>
+        <el-header class="header">头部</el-header>
+        <el-main class="main">主体</el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
+<style lang="less" scoped>
 
-<style>
-
+  .left{
+    // width:200px;
+    // height: 100vh;
+    // background-color: #ccc;
+  }
 </style>
